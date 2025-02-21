2d861c40518e57b482e2e64b056165408c73a931
randengrims <randengrimshaw@gmail.com>
2025-02-20 22:13:05 -0700

    added to movie form page

--- DateMe/Models/Application.cs
+++ DateMe/Models/Application.cs
@@ -9,8 +9,7 @@ namespace DateMe.Models
         [Required]
         public int MovieId { get; set; }
 
-        [Required(ErrorMessage = "Category is required.")]
-        public string Category { get; set; }
+        public string? Category { get; set; }
 
         [Required(ErrorMessage = "Title is required.")]
         public string Title { get; set; }
@@ -18,15 +17,16 @@ namespace DateMe.Models
         [Required(ErrorMessage = "Year is required."), Range(1888, 2026, ErrorMessage = "Year must be between 1888 and 2026.")]
         public int Year { get; set; } = 1888;
 
-        [Required(ErrorMessage = "Director is required.")]
-        public string Director { get; set; }
+        public string? Director { get; set; }
 
-        [Required(ErrorMessage = "Rating is required.")]
-        public string Rating { get; set; }
+        public string? Rating { get; set; }
 
         // Optional fields
         public bool? Edited { get; set; }
         public string? LentTo { get; set; }
+
+        [Required(ErrorMessage = "Must put whether the movie was copied to Plex or not.")]
+        public bool CopiedToPlex { get; set; }
 
         [StringLength(25, ErrorMessage = "Notes cannot exceed 25 characters.")]
         public string? Notes { get; set; }
